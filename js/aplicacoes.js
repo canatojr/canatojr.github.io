11e21a3de7a9d5df233f2af689b8eb884cc587e8
Osvaldo Canato Jr <canatojr@gmail.com>
2024-01-02 09:56:26 -0800

    Update aplicacoes.js

--- js/aplicacoes.js
+++ js/aplicacoes.js
@@ -146,7 +146,7 @@ App.aplicacoes = (function ()
         mensagem,
         "#0fc",
         "Bold 16px Trebuchet MS",
-        objCanvas.canvasWidth/2 + objCanvas.canvasWidth/22 + ((BASE/18)),
+        objCanvas.canvasWidth/2 + objCanvas.canvasWidth/22 + ((BASE/20)),
         170
       ]);
 
